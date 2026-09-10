9000e24a2d7d86979438890352b8450be34eeb0f
Alok-in-action <itsalokkhamora@gmail.com>
2026-09-10 13:39:52 +0530

    Remove exposed secrets to pass Netlify scanner

--- backend/database.py
+++ backend/database.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:Alokkhamora@db.wtufljxwlkvovjbgqgbo.supabase.co:5432/postgres")
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./medikiosk.db")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
